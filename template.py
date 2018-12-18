abd1aaa081efc92e243c39636b3a7a3c1e0efcef
Jaikrishna Tanjore Sukumar <t.s.jaikrishna@gmail.com>
2018-12-18 10:03:16 -0500

    update template

--- template.py
+++ template.py
@@ -3,9 +3,12 @@
 import sys
 import re
 
-test_inp = len(sys.argv) > 1 and sys.argv[1] == '-t'
-dayn = re.match(r'.*day(\d+)\.py', sys.argv[0]).group(1)
-input_filename = 'inp' + dayn + ('_' if test_inp else '') + '.txt'
+if len(sys.argv) > 1:
+    input_filename = sys.argv[1]
+else:
+    dayn = re.match(r'.*day(\d+)\.py', sys.argv[0]).group(1)
+    input_filename = 'inp' + dayn + '.txt'
 
 with open(input_filename) as f:
-    nums = map(int, f.read().strip())
+    for line in f:
+        pass
